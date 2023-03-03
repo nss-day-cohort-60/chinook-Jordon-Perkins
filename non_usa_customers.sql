271c73186b13e6b419e6e3e565746c59b6f0cfd1
Jordon Perkins <jordon.rubel@gmail.com>
2023-03-03 11:49:19 -0600

    1 Done

---
+++ non_usa_customers.sql
@@ -0,0 +1,12 @@
+Provide a query showing Customers who are not in the US. The resultant table should include:
+
+Customer's full name
+Customer Id,
+Customer's country
+
+
+
+
+SELECT FirstName, LastName, Country, CustomerId
+FROM Customer
+WHERE Country NOT LIKE "%USA%"
