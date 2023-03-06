1b9df743a777dff59542b8a0d5db790c552dea5e
Jordon Perkins <jordon.rubel@gmail.com>
2023-03-06 12:56:54 -0600

    actually done

--- country_invoices.sql
+++ country_invoices.sql
@@ -1,6 +1,6 @@
 Provide a query that shows the total number of invoices per country.
 HINT: GROUP BY
 
-SELECT Distinct SUM(InvoiceId), BillingCountry AS Country
+SELECT COUNT(InvoiceId), BillingCountry AS Country
 FROM Invoice
 GROUP BY BillingCountry
