8de78eb5379b76c63684dd8327fc956295b230ab
Jordon Perkins <jordon.rubel@gmail.com>
2023-03-06 14:09:57 -0600

    done

---
+++ invoices_line_item_count.sql
@@ -0,0 +1,11 @@
+Provide a query that shows all Invoices. The resultant table should include:
+InvoiceId
+The total number of line items on each invoice
+
+
+
+SELECT i.InvoiceId, COUNT(il.InvoiceId) AS Line_Items
+FROM Invoice AS i
+JOIN InvoiceLine AS il
+ON i.InvoiceId = il.InvoiceId
+GROUP BY i.InvoiceId
